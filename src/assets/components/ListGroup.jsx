0f393d8f67c191ed10dca83b933054258ad7219d
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-09-15 07:37:47 +0545

    handleClick

--- src/assets/components/ListGroup.jsx
+++ src/assets/components/ListGroup.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 const ListGroup = () => {
   let items = ["Nepal", "USA", "India", "China", "Russia"];
+  const handleClick = (event) => {
+    console.log("Clicked");
+  };
   return (
     <>
       {items.length === 0 && <p>Items not found</p>}
-      {items.map((items) => (
-        <li key={items}>{items}</li>
-      ))}
+      <ul className="list-group">
+        {items.map((items, index) => (
+          <li className="list-group-item" key={items} onClick={handleClick}>
+            {items}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
